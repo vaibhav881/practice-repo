b89b41a82abcacc957e4433af6b0d54cb22de0af
vaibhav881 <mvaibhav002@gmail.com>
2020-11-06 18:49:58 +0530

    sucessfully merged

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
 hungry = input("Are you hungry: ")
-<<<<<<< HEAD
 if hungry == "Yes" and hungry == "Yo":
-=======
-if hungry == "Yes" and hungry == "Y":
->>>>>>> 862f804ab3fbae2a5b3d28eb58feade84ad1edcb
     print("Eat Samosa.")
     print("Eat pizza.")
     print("Eat Burger.")
 else:
     print("Do Your HomeWork.")
+ 
